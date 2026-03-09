e056b3b7c96a0768e792b9b81c1c31953988653d
yemrekaymak <yunsemrekaymak9@gmail.com>
2026-03-10 01:40:07 +0300

    fix: program.cs

--- Program.cs
+++ Program.cs
@@ -114,4 +114,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated(); 
+}
 app.Run();
